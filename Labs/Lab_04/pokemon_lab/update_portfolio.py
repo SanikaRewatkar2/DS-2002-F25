26ac3474e9d9fbeb3cac96aaf27aedc689dfb3dc
SanikaRewatkar2 <sanika_rewatkar@icloud.com>
2025-10-21 20:05:17 -0400

    Update portfolio function doen my god that took longer than i though

--- Labs/Lab_04/pokemon_lab/update_portfolio.py
+++ Labs/Lab_04/pokemon_lab/update_portfolio.py
@@ -3,6 +3,8 @@
 import json
 import pandas as pd # if you get the error "pandas not found waaaah", change the interpreter
 import os # for directory stuffs
+import sys # for error printing
+import csv # for file writing
 
 # load lookup dir function
 def _load_lookup_dir(lookup_dir): # load json prices
@@ -46,7 +48,7 @@ def _load_inventory_dir(inventory_dir):
             temp_inv_df = pd.read_csv("Labs/Lab_04/pokemon_lab/{inventory_dir}/{filename}".format(inventory_dir=inventory_dir, filename=filename)) # read csv https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html#pandas.read_csv
             #print(temp_inv_df) #aaa
             inventory_data.append(temp_inv_df) # append
-    print(inventory_data)
+    #print(inventory_data)
     # check if empty
     if len(inventory_data) == 0:
         return pd.DataFrame() # return empty df ( I wanted to use the "new" keyword but this isn't java aaa )
@@ -61,7 +63,48 @@ def _load_inventory_dir(inventory_dir):
     #print(inventory_df)
     return inventory_df
 
+# now the final function
+def update_portfolio(inventory_dir, lookup_dir, output_file):
+    # call the other functions
+    my_inventory_df = _load_inventory_dir(inventory_dir)
+    my_lookup_df = _load_lookup_dir(lookup_dir)
+    # check for empty inventory df https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.empty.html
+    filepath = "Labs/Lab_04/pokemon_lab/{output_file}".format(output_file=output_file) # construct filepath
+    if my_inventory_df.empty:
+        # create csv
+        headers = ["index", "card_id", "card_name", "card_number", "set_id", "set_name", "binder_name", "page_number", "slot_number", "card_market_value"]
+        with open(filepath, "w", newline='') as f: # https://www.pythontutorial.net/python-basics/python-write-csv-file/
+            writer = csv.writer(f)
+            writer.writerow(headers)
+        print("Error: Empty Inventory!", file=sys.stderr) # https://pythonhow.com/how/print-to-stderr-in-python/
+        return
+    # otherwise, full steam ahead
+    #print(my_inventory_df)
+    #print(my_lookup_df)
+    final_inventory_df = my_inventory_df.merge(my_lookup_df, how='left', on='card_id') # https://pandas.pydata.org/docs/reference/api/pandas.merge.html
+    # now I need to do even more cleanup
+    final_inventory_df = final_inventory_df.rename(columns={"set_id_x": "set_id"})
+    final_cols = ["card_id", "card_name", "card_number", "set_id", "set_name", "binder_name", "page_number", "slot_number", "card_market_value"] # final cols v1
+    final_inventory_df = final_inventory_df[final_cols]
+    # moar cleanup
+    # fill NaN market value with zeroes
+    final_inventory_df['card_market_value'] = final_inventory_df['card_market_value'].fillna(0.0)
+    # fill NaN set names with "NOT FOUND"
+    final_inventory_df['set_name'] = final_inventory_df['set_name'].fillna("NOT_FOUND")
+    # make index
+    # construct shared keyyyyyyy
+    indices = []
+    for index, row in final_inventory_df.iterrows(): # https://note.nkmk.me/en/python-pandas-dataframe-for-iteration/
+        indices.append("{binder_name}{page_number}{slot_number}".format(binder_name=row["binder_name"], page_number=row["page_number"], slot_number=row["slot_number"]))
+    # otherwise give a shared key https://note.nkmk.me/en/python-pandas-assign-append/
+    final_inventory_df.insert(0, "index", indices) 
+    print(final_inventory_df) # SANIRY CHECK
+    final_cols.insert(0, "index")
+    # now csv mcgee stuff
+    final_inventory_df.to_csv(filepath, index=False) # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_csv.html
+    print("Card Portfolio Successfully Updated!")
 
 if __name__ == "__main__":
-    #print(_load_lookup_dir("card_inventory_test"))
-    print(_load_inventory_dir("card_inventory_test"))
+    #print(_load_lookup_dir("card_set_lookup_test"))
+    #print(_load_inventory_dir("card_inventory_test"))
+    update_portfolio("card_inventory_test", "card_set_lookup_test", "example.csv")
